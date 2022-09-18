01c6a9bea975ce434cb7af4958a4b0d4b180f121
Milleer <104246250+Milleer@users.noreply.github.com>
2022-09-18 14:18:02 +0700

    Delete GIT initiated.py
    File delated

--- GIT initiated.py
+++
@@ -1,6 +0,0 @@
-# git initiated
-# git config --global user.name "your name"
-# git config --global user.email "your e-mail"
-# git config --global push.default matching
-# git config --global alias.co checkout
-# git init 
