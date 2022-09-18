74a745587f88e3238585e7e6d715e70921dd5b11
Milleer <104246250+Milleer@users.noreply.github.com>
2022-09-18 14:18:22 +0700

    Add files via upload

---
+++ GITinitiated.py
@@ -0,0 +1,6 @@
+# git initiated
+# git config --global user.name "your name"
+# git config --global user.email "your e-mail"
+# git config --global push.default matching
+# git config --global alias.co checkout
+# git init 
